ecb78a5164ed146b160ce8635c53e0bbbf2ebf4f
Brendan Koetting <brendank575@gmail.com>
2022-10-15 17:24:23 -0600

    announcement

--- chamber/scripts/dates.js
+++ chamber/scripts/dates.js
@@ -6,3 +6,7 @@ d = new Date();
 let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][d.getDay()];
 let month = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "November", "September", "October", "November", "December"][d.getMonth()]
 document.getElementById("todaysDate").textContent = day + ", " + String(d.getDate()) + " " + month + " " + d.getFullYear();
+
+if (d.getDay() == 1 || d.getDay() == 2) {
+    document.getElementById("announcement").style.display = "block";
+}
